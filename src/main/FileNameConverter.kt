96d4a6eb38d55630ef388d476f9e46b69c32166a
kenji matsuoka <egs@firespeed.org>
2020-11-19 02:14:06 +0900

    Create FileNameConverterTest.

--- src/main/FileNameConverter.kt
+++ src/main/FileNameConverter.kt
@@ -2,16 +2,33 @@ package main
 
 import java.io.File
 import java.text.SimpleDateFormat
+import java.util.*
 
 class FileNameConverter(file: File) {
     val lastUpdatedDateTime: String
     val lastUpdatedDate: String
     val originalFileName: String
+    val folderNumnber: Int
     val inMic: Boolean
     val outMic: Boolean
+    val year:Int
+    val month:Int
+    val day:Int
 
+    val hour:Int
+    val minute:Int
+    val second:Int
     init {
+        year = 0
+        month = 0
+        day= 0
+        hour = 0
+        minute = 0
+        second = 0
+        folderNumnber = 0
         originalFileName = file.name
+        val cal = Calendar.getInstance()
+        cal.time = Date(file.lastModified())
         lastUpdatedDateTime = DATE_TIME_SDF.format(file.lastModified())
         lastUpdatedDate = DATE_SDF.format(file.lastModified())
         inMic = MS_REGEX.matches(originalFileName)
---
+++ src/test/FileNameConverterTest.kt
@@ -0,0 +1,62 @@
+package test
+
+import main.FileNameConverter
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+import java.nio.file.Path
+import java.util.*
+
+internal class FileNameConverterTest {
+    @Test
+    fun testFileNameTestInMic(@TempDir dir: Path) {
+        val fourCh = dir.resolve("4CH")
+        val folder1 = fourCh.resolve("FOLDER01")
+        val folder2 = fourCh.resolve("FOLDER02")
+        val targetFileMS11 = folder1.resolve("SR001MS.WAV")
+        val targetFileXY11 = folder1.resolve("SR001XY.WAV")
+        val targetFileMS21 = folder2.resolve("SR001MS.WAV")
+        val targetFileXY21 = folder2.resolve("SR001XY.WAV")
+        val targetFileMS11File = File(targetFileMS11.toUri())
+        val targetFileXY11File = File(targetFileXY11.toUri())
+        val targetFileMS21File = File(targetFileMS21.toUri())
+        val targetFileXY21File = File(targetFileXY21.toUri())
+
+        val cal = Calendar.getInstance()
+        cal.set(2019, 5,1, 2,3,4 )
+        targetFileMS11File.setLastModified(cal.timeInMillis)
+        val fileNameConverterMS11 = FileNameConverter(targetFileMS11File)
+        val fileNameConverterXY11 = FileNameConverter(targetFileXY11File)
+        val fileNameConverterMS21 = FileNameConverter(targetFileMS21File)
+        val fileNameConverterXY21 = FileNameConverter(targetFileXY21File)
+
+        assertTrue(fileNameConverterMS11.inMic)
+        assertFalse(fileNameConverterMS11.outMic)
+        assertFalse(fileNameConverterXY11.inMic)
+        assertTrue(fileNameConverterXY11.outMic)
+
+        assertTrue(fileNameConverterMS21.inMic)
+        assertFalse(fileNameConverterMS21.outMic)
+        assertFalse(fileNameConverterXY21.inMic)
+        assertTrue(fileNameConverterXY21.outMic)
+
+        assertEquals(fileNameConverterMS11.folderNumnber, 1)
+        assertEquals(fileNameConverterXY11.folderNumnber, 1)
+        assertEquals(fileNameConverterMS21.folderNumnber, 2)
+        assertEquals(fileNameConverterXY21.folderNumnber, 2)
+
+        assertEquals(fileNameConverterMS11.originalFileName, "SR001MS.WAV")
+        assertEquals(fileNameConverterXY11.originalFileName, "SR001XY.WAV")
+        assertEquals(fileNameConverterMS21.originalFileName, "SR002MS.WAV")
+        assertEquals(fileNameConverterXY21.originalFileName, "SR002XY.WAV")
+        assertEquals(fileNameConverterXY21.year, 2019)
+        assertEquals(fileNameConverterXY21.month, 5)
+        assertEquals(fileNameConverterXY21.day, 1)
+        assertEquals(fileNameConverterXY21.hour, 2)
+        assertEquals(fileNameConverterXY21.minute, 3)
+        assertEquals(fileNameConverterXY21.second, 4)
+
+
+    }
+}
