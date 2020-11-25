07649022acd93f409f7d7349d81d23cc2319e94f
kenji matsuoka <egs@firespeed.org>
2020-11-26 02:16:10 +0900

    Implement Directory Resolver.

--- src/main/createDirectory/DirectoryResolver.kt
+++ src/main/createDirectory/DirectoryResolver.kt
@@ -4,6 +4,31 @@ import java.nio.file.Path
 
 class DirectoryResolver {
     fun directoryResolve(path:Path): Path {
-        return path
+        val file = path.toFile()
+        if(!file.exists()){
+            file.mkdirs()
+            return path
+        }
+        if(file.isDirectory){
+            return path
+        }
+        return getNumberedDirectory(path)
+    }
+    private fun getNumberedDirectory(path:Path): Path{
+        var index = 1
+        while(index<Int.MAX_VALUE){
+            val checkPath = path.parent.resolve( "%s (%d)".format(path.last().toString() ,index))
+            val checkFile = checkPath.toFile()
+            if(!checkFile.exists()){
+                checkFile.mkdirs()
+                return checkPath
+            }
+            if(checkFile.isDirectory){
+                return checkPath
+            }
+            index++
+        }
+        throw (FileAlreadyExistsException(path.toFile()))
+
     }
 }
